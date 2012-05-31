6fd294ed0322bd13fa90dd55394dd0e09d37e3ae
Pedro Melo <melo@simplicidade.org>
2012-05-31 10:38:59 +0100

    Fix ABSTRACT, cleanup VERSION, AUTHORITY
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/DBIx/Nesting.pm
+++ lib/DBIx/Nesting.pm
@@ -1,11 +1,11 @@
 package DBIx::Nesting;
 
-# ABSTRACT: a very cool module
+# ABSTRACT: Take multi-join DBI result and nest it into a tree
+# VERSION
+# AUTHORITY
 
 use strict;
 use warnings;
 
-# VERSION
-# AUTHORITY
 
 1;
